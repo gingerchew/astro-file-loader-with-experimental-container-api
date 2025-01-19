ef0166b5543ac6890992ce04c41468ba5d85d17c
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-19 13:43:00 -0500

    initial commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { glob } from 'astro/loaders';
+import { experimental_AstroContainer } from 'astro/container';
 import { defineCollection, z } from 'astro:content';
-
+import type { Loader } from 'astro/loaders';
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -15,4 +16,33 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const AstroFileLoader = async ({ base, pattern }: Record<string, string>): Promise<Loader> => {
+	const container = await experimental_AstroContainer.create()
+	return {
+		name: 'astro-file-loader',
+		load: async (context) => {
+
+			const fromGlob = import.meta.glob('./content/astro/*.astro');
+			
+			for (const [id, entry] of Object.entries(fromGlob)) {
+				// @ts-ignore
+				const { default: Post } = await entry();
+				const renderedEl = await container.renderToString(Post)
+				context.store.set({
+					id,
+					data: { id },
+					rendered: {
+						html: renderedEl
+					}
+					// body: (await entry()) as string
+				})
+			}
+		},
+	}
+}
+
+const astro = defineCollection({
+	loader: await AstroFileLoader({ base: './content/astro', pattern: '*.astro' }),
+})
+
+export const collections = { blog, astro };
